8961534ba4c8c43ce17a30c1735714b8a44014c5
Odisley <odisleyribeirosilva@hotmail.com>
2023-10-29 20:02:42 -0300

    Commit das opções de editar as consultas da agenda

--- core/models.py
+++ core/models.py
@@ -18,3 +18,5 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%y %H:%Mhrs')
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
